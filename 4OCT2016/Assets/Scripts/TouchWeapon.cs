686d46c10a4f4abdfa1f6cc76b4ba9766afced11
Evan <daley.evan@gmail.com>
2016-10-04 20:11:55 -0700

    Add intro and create final build

---
+++ 4OCT2016/Assets/Scripts/TouchWeapon.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchWeapon : MonoBehaviour {
+
+	void Update ()
+	{
+		if(Input.touchCount > 0 || Input.GetMouseButton(0))
+		{
+			//the user touched the screen, get position
+
+		}
+	}
+}
